5be07a69fdead0fda70e0e288957ad98ddb3163d
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-01-21 16:59:00 +0000

    updated save button code and added functions for updated colours and to show saved events

--- challenge/Script.js
+++ challenge/Script.js
@@ -1,29 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log("Document is ready."); // Check if document is ready
+  // Display current date in the header
+  $("currentDay").text(dayjs().format("dddd, MMMM D"));
 
+  // let currentDay= $("currentDay");
+  // let now=dayjs().format('dddd, MMMM Do D');
+  // // currentDay.text(now);
+  // $("currentDay").html(currentDay);
 
-let currentDay= $("currentDay");
-let now=dayjs().format('dddd, MMMM Do D');
-// currentDay.text(now);
-$("currentDay").html(currentDay);
+  // Function to update time block colors based on current time
+  function updateHourlyColors() {
+    let currentHour = dayjs().hour();
 
-let timeBlock =[
-  { time: "9 AM", event: "" },
-  { time: "10 AM", event: "" },
-  { time: "11 AM", event: "" },
-  { time: "12 PM", event: "" },
-  { time: "1 PM", event: "" },
-  { time: "2 PM", event: "" },
-  { time: "3 PM", event: "" },
-  { time: "4 PM", event: "" },
-  { time: "5 PM", event: "" },
-];
+    $(".row").each(function () {
+      let hourBlock = parseInt($(this).find(".col-md-1").text().trim());
 
+      if (hourBlock < currentHour) {
+        $(this).removeClass("present future").addClass("past");
+      } else if (hourBlock === currentHour) {
+        $(this).removeClass("past future").addClass("present");
+      } else {
+        $(this).removeClass("past present").addClass("future");
+      }
+    });
+  }
 
+  // Function to load saved events from localStorage
+  function loadEvents() {
+    $(".row").each(function () {
+      let hourBlock = $(this).find(".col-md-1").text().trim();
+      let savedEvent = localStorage.getItem(hourBlock);
 
-$(".saveBtn").on("click", function(event){
-  console.log(this);
-  // event.preventDefault();
-$("#hourOfDay").append("#timeBlockText");
-})
+      if (savedEvent) {
+        $(this).find("#timeBlockText").value(savedEvent);
+      }
+    });
+  }
+
+// Function to save events to localStorage
+
+// Update colors on page load
+updateHourlyColors();
+
+// Load saved events on page load
+loadEvents();
+
+  // function for timeslots
+  // $("hourOfDay").each(function()){
+
+  // }
+// Save events on save button click
+  $(".saveBtn").on("click", function(){
+    saveEvents();
+    // console.log(this);
+    // event.preventDefault();
+  // $("#hourOfDay").append("#timeBlockText");
+  });
 });
