844bdb30557f46926bd76aad97bf15240d463484
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-01-21 16:51:06 +0000

    added function to update time blocks based on time

--- challenge/Script.js
+++ challenge/Script.js
@@ -1,14 +1,29 @@
 $(document).ready(function() {
   console.log("Document is ready."); // Check if document is ready
-});
+
 
 let currentDay= $("currentDay");
 let now=dayjs().format('dddd, MMMM Do D');
 // currentDay.text(now);
 $("currentDay").html(currentDay);
 
-// let timeBlock =
+let timeBlock =[
+  { time: "9 AM", event: "" },
+  { time: "10 AM", event: "" },
+  { time: "11 AM", event: "" },
+  { time: "12 PM", event: "" },
+  { time: "1 PM", event: "" },
+  { time: "2 PM", event: "" },
+  { time: "3 PM", event: "" },
+  { time: "4 PM", event: "" },
+  { time: "5 PM", event: "" },
+];
 
-$(".saveBtn").on("click", function(){
-  
+
+
+$(".saveBtn").on("click", function(event){
+  console.log(this);
+  // event.preventDefault();
+$("#hourOfDay").append("#timeBlockText");
 })
+});
